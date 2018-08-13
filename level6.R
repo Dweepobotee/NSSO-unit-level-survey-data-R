835ddb4347fdb972b74f9add79f595a6c259ab3e
Dweepobotee Brahma <dweepobotee@gmail.com>
2018-08-12 22:18:48 -0400

    Update level6.R

--- level6.R
+++ level6.R
@@ -1,5 +1,5 @@
 ## Level 6
-
+## Importing relevant parts of the 'common id'
 level6<-read_fwf(file="<Insert path name to level 6 txt file>", fwf_cols(fsuslno=c(4,8), sector=c(15,15), subblockno=c(32,32),sssno=c(33,33),hhno=c(34,35),level=c(36,37),filler=c(38,38),
                                                 hospslno=c(39,40), personid=c(41,42),age=c(43,45),freemeddevice=c(46,46),package_component=c(47,53),doctorfee=c(54,60),
                                                 medicine=c(61,67),diagnostictests=c(68,73),bedcharges=c(74,80),otherexpenses=c(81,87),totalmedexpenses=c(88,94),
