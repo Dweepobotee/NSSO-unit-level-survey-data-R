4e04b96734553a965a4ad5ee76e116b8576ffd87
Dweepobotee Brahma <dweepobotee@gmail.com>
2018-08-18 23:02:15 -0400

    Update level9.R

--- level9.R
+++ level9.R
@@ -1,5 +1,5 @@
 ## Level 9
-
+## Importing relevant parts of 'common id'
 level9<-read_fwf(file="<Insert file name of txt file for level 9>",fwf_cols(fsuslno=c(4,8), sector=c(15,15), subblockno=c(32,32),sssno=c(33,33),hhno=c(34,35),level=c(36,37),filler=c(38,38),
                                                personid=c(41,42),age=c(43,45),numlivingson=c(46,47),numlivingdaughter=c(48,49),stateecondependence=c(50,50),
                                                numdependents=c(51,52),personfinanciallysupold=c(53,53),livingarangement=c(54,54),physicalmobility=c(55,55),
