968884d291110c70a4fce1119a7e2c8419fda255
Dweepobotee Brahma <dweepobotee@gmail.com>
2018-08-18 23:00:50 -0400

    Add files via upload

---
+++ level9.R
@@ -0,0 +1,10 @@
+## Level 9
+
+level9<-read_fwf(file="R71250L09.txt",fwf_cols(fsuslno=c(4,8), sector=c(15,15), subblockno=c(32,32),sssno=c(33,33),hhno=c(34,35),level=c(36,37),filler=c(38,38),
+                                               personid=c(41,42),age=c(43,45),numlivingson=c(46,47),numlivingdaughter=c(48,49),stateecondependence=c(50,50),
+                                               numdependents=c(51,52),personfinanciallysupold=c(53,53),livingarangement=c(54,54),physicalmobility=c(55,55),
+                                               personhelping=c(56,56),currenthealth=c(57,57),changeinhealth=c(58,58),nss=c(127,129),nsc=c(130,132), mlt=c(133,142)),
+                 col_types=cols(fsuslno=col_character(), sector=col_character(), subblockno=col_character(),sssno=col_character(),hhno=col_character(),level=col_character(), filler=col_character(),
+                                personid=col_character(),age=col_integer(),numlivingson=col_integer(), numlivingdaughter=col_integer(),stateecondependence=col_integer(),
+                                numdependents=col_integer(),personfinanciallysupold=col_integer(),livingarangement=col_integer(),physicalmobility=col_integer(),
+                                personhelping=col_integer(), currenthealth=col_integer(),changeinhealth=col_integer(),nss=col_character(), nsc=col_character(), mlt=col_character()))
