a4e8bc29f7e51244499cfc6f65f1814a55ca79c8
Dweepobotee Brahma <dweepobotee@gmail.com>
2018-08-18 23:01:16 -0400

    Update level9.R

--- level9.R
+++ level9.R
@@ -1,6 +1,6 @@
 ## Level 9
 
-level9<-read_fwf(file="R71250L09.txt",fwf_cols(fsuslno=c(4,8), sector=c(15,15), subblockno=c(32,32),sssno=c(33,33),hhno=c(34,35),level=c(36,37),filler=c(38,38),
+level9<-read_fwf(file="",fwf_cols(fsuslno=c(4,8), sector=c(15,15), subblockno=c(32,32),sssno=c(33,33),hhno=c(34,35),level=c(36,37),filler=c(38,38),
                                                personid=c(41,42),age=c(43,45),numlivingson=c(46,47),numlivingdaughter=c(48,49),stateecondependence=c(50,50),
                                                numdependents=c(51,52),personfinanciallysupold=c(53,53),livingarangement=c(54,54),physicalmobility=c(55,55),
                                                personhelping=c(56,56),currenthealth=c(57,57),changeinhealth=c(58,58),nss=c(127,129),nsc=c(130,132), mlt=c(133,142)),
