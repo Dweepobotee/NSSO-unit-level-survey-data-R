090c67c17d9c6ef3e3a8ddf4e5cdbb9bc056e3ba
Dweepobotee Brahma <dweepobotee@gmail.com>
2018-08-25 23:36:05 -0400

    Update level10.R

--- level10.R
+++ level10.R
@@ -1,6 +1,6 @@
 ## Level 10
 
-level10<-read_fwf(file="",fwf_cols(fsuslno=c(4,8), sector=c(15,15), subblockno=c(32,32),sssno=c(33,33),hhno=c(34,35),level=c(36,37),filler=c(38,38),
+level10<-read_fwf(file="<Insert file name for level 10 txt file>",fwf_cols(fsuslno=c(4,8), sector=c(15,15), subblockno=c(32,32),sssno=c(33,33),hhno=c(34,35),level=c(36,37),filler=c(38,38),
                                                 personid=c(41,42),age=c(43,45),pregnant=c(46,46),pregnancyslno=c(47,47),tetanus=c(48,48),IFA=c(49,49),prenatal=c(50,50),
                                                 natureprenatal=c(51,51),expprenatal=c(52,59),outcome=c(60,60),plcdelivery=c(61,61),postnatal=c(62,62),naturepostnatal=c(63,63),
                                                 exppostnatal=c(64,71),nss=c(127,129),nsc=c(130,132),mlt=c(134,142)),
